17db992fae838e9149c5ff91917190f1064a1b11
DockTownMayor <theunknownperson77@gmail.com>
2016-08-28 22:48:32 -0400

    Update macchangewarning.sh

--- Singular-Files/macchangewarning.sh
+++ Singular-Files/macchangewarning.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# We believe here that your interface is wlan0 and that you have spd-say installed!
 warnColor='\033[0;31m';
 colorReset='\033[0m';
 permanentMac=$(macchanger -s wlan0|grep "Permanent"|awk '{print $3}');
@@ -12,6 +13,8 @@ sleep 1;
 clear;
 done
 printf "${warnColor}Mac address changed unexpectedly!${colorReset}\n"
+spd-say "Mac address changed unexpectedly!"
 else
 printf "${warnColor}Mac was never changed from the start!${colorReset}\n"
+spd-say "Mac was never changed from the start!"
 fi
